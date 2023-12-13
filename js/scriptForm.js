e9ded2c968ae7ad7f637ba437c18710cc0aca0b4
Vladislav <vladislav.malchevskiy.work@gmail.com>
2023-12-14 00:59:00 +0300

    add validate form

---
+++ js/scriptForm.js
@@ -0,0 +1,64 @@
+const name = document.getElementsByName("name");
+const phone = document.getElementsByName("phone");
+const email = document.getElementsByName("E-mail");
+
+const formOfPage = document.getElementById("formMain");
+const formOfOrder = document.getElementById("formOrder");
+const formOfConsultation = document.getElementById("formConsultation");
+
+
+function generateError(text, i) {
+    let error = document.createElement('div');
+    error.className = 'error';
+    error.style.color = 'red';
+    error.innerHTML = text;
+
+    let inputs = document.getElementsByTagName("input");
+    inputs.item(i).style.borderStyle = 'solid';
+    inputs.item(i).style.borderWidth = '1px';
+    inputs.item(i).style.borderColor = 'red';
+
+    return error;
+}
+function validate(form){
+
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        let errors = form.getElementsByClassName('error');
+
+        while (errors.length > 0){
+            errors[0].remove();
+        }
+
+        let inputs = document.getElementsByTagName("input");
+        for (let i = 0; i < inputs.length; i++){
+            inputs.item(i).style.borderColor = 'gray';
+            inputs.item(i).style.borderStyle = 'solid';
+            inputs.item(i).style.borderWidth = '1px';
+        }
+
+        for (let i = 0; i < name.length; i++){
+           if(form.contains(name[i])){
+               if (!name[i].value) {
+                   form.insertBefore(generateError("Введите имя", 3*i + 0), name[i]);
+               }
+               if (!email[i].value) {
+                   form.insertBefore(generateError("Введите свою почту", 3*i + 2), email[i]);
+               }
+               if (!phone[i].value) {
+                   form.insertBefore(generateError("Введите номер телефона", 3*i + 1), phone[i]);
+               }
+           }
+        }
+    })
+}
+
+validate(formOfPage);
+validate(formOfConsultation);
+validate(formOfOrder);
+
+
+
+
+
